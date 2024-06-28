ada7744145684cbd5325ae52169e43d9f634b0a0
RishavGaur <rishav021gaur@gmail.com>
2024-06-28 13:18:36 +0530

    triangle.py

---
+++ Basic/Assignment-1/triangle.py
@@ -0,0 +1,6 @@
+# Write a python script to calculate area of triangle, Length of the sides are given by user.
+
+breadth=float(input("Enter the breadth of the triangle:"))
+height= float(input("Enter the heigth of the triangle:"))
+
+print("Area of Triangle=",(1/2*breadth*height))
