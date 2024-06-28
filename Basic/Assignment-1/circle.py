c73d3cdd242cbc1497f1c8776a3c37b2349d3c4d
RishavGaur <rishav021gaur@gmail.com>
2024-06-28 12:56:36 +0530

    Finding Area of Circle

---
+++ Basic/Assignment-1/circle.py
@@ -0,0 +1,4 @@
+# Write a python script to calculate area of circle, radius is taken from user.
+from math import pi,pow
+radius= float(input("Enter the radius of Circle:"))
+print("Area of Circle=",pi*pow(radius,2))
