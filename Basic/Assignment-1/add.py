c0cd7e72394d8c3450e69f0c3384938172eb4efb
RishavGaur <rishav021gaur@gmail.com>
2024-06-28 12:48:11 +0530

    Sum of two numbers

---
+++ Basic/Assignment-1/add.py
@@ -0,0 +1,3 @@
+# Write a python script to add two numbers(integers) taken from user through keyboard.
+sum= int(input("Enter first Number:")) + int(input("Enter second Number:"))
+print("Sum of both Numbers=",sum)
