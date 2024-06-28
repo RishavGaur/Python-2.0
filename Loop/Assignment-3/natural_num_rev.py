c945f3e32fec22b95ca29158011e18a9257bb478
RishavGaur <rishav021gaur@gmail.com>
2024-06-28 16:19:19 +0530

    Printing Natural Numbers in Reverse Order

---
+++ Loop/Assignment-3/natural_num_rev.py
@@ -0,0 +1,7 @@
+''' Write a python script to print first 10 Natural Numbers in reverse order '''
+
+print("First 10 Reverse Natural Numbers are: ",end='')
+i=10
+while(i>0):
+    print(i,end=',')
+    i-=1
