01c2d4dbcd28be02e1f4b9eb5c2f9035f23680f5
RishavGaur <rishav021gaur@gmail.com>
2024-06-28 17:22:44 +0530

    Corrections

--- Loop/Assignment-3/sum_nth_odd.py
+++ Loop/Assignment-3/sum_nth_odd.py
@@ -1,12 +1,11 @@
 ''' Write a python script to calculate sum of first N Odd Natural Numbers. Value of N is taken from user '''
 
 nth= int(input("Enter nth Number:"))
+
 sum=0
 i=1
-count=0
-while count<=nth:
+while i<=nth:
     if i%2!=0:
         sum+=i
-        count+=1
     i+=1
 print("Sum of nth Odd Natural Numbers=",sum)
