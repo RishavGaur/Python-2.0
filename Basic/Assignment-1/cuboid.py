250c2dfa1c0388a6274fb5af6576a7fdbab8a46c
RishavGaur <rishav021gaur@gmail.com>
2024-06-28 13:04:14 +0530

    Volume of the Cuboid

---
+++ Basic/Assignment-1/cuboid.py
@@ -0,0 +1,8 @@
+# Write a python script to calculate volume of a cuboid, dimensions(float values) taken from user.
+
+# Taking Dimensions of the Cuboid as input
+length= float(input("Enter length of the Cuboid:"))
+breadth= float(input("Enter breadth of the Cuboid:"))
+heigth= float(input("Enter heigth of the Cuboid:"))
+
+print("Volume of the Cuboid=",length*breadth*heigth)
