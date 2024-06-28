1d469746470ef005651434ea63c3cad46959b4ee
RishavGaur <rishav021gaur@gmail.com>
2024-06-28 13:33:52 +0530

    Checking Number is Positive, Negative or Zero

---
+++ Decision-Control/Assignment-2/pos_zer_neg.py
@@ -0,0 +1,11 @@
+# Write a python script to check whether a given number is Positive, Negative or Zero.
+
+num= int(input("Enter a Number:"))
+
+# Now, Checking the conditions for the num is +ve, -ve or 0.
+if num>0:
+    print("Given Num. is Positive.")
+elif num<0:
+    print("Given Num is Negative.")
+else:
+    print("Given Number is Zero")
