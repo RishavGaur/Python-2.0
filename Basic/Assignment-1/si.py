972a6c234ad60f6250b6679c8254415d6a059082
RishavGaur <rishav021gaur@gmail.com>
2024-06-28 13:09:14 +0530

    Calculating Simple Interest

---
+++ Basic/Assignment-1/si.py
@@ -0,0 +1,8 @@
+# Write a python script to calculate Simple Interest, data required should be taken from user
+
+# Taking data to calculate si
+p= float(input("Enter Principle Amount:"))
+r= float(input("Enter Rate of Interest:"))
+t= float(input("Enter the Period of Time:"))
+
+print("Simple Interest=",(p*r*t)/100)
