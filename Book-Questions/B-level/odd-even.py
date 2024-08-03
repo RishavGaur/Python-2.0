f4bf52e80623e1308ce0e67ddbfec08232ef4918
RishavGaur <rishav021gaur@gmail.com>
2024-08-03 12:41:55 +0530

    finding out a number is even or odd

---
+++ Book-Questions/B-level/odd-even.py
@@ -0,0 +1,5 @@
+'''
+Any integer is input through the keyboard. Write a program to 
+find out whether it is an odd number or even number.
+'''
+print("It's Even Number") if int(input("Enter a Number: "))%2==0 else print("It's Odd Number")
