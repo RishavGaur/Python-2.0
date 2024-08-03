f0621ab752485b4642c2fc2158cf065a6daa76f2
RishavGaur <rishav021gaur@gmail.com>
2024-08-03 11:17:36 +0530

    Converting temperature from fahrenheit to centigrade

---
+++ Book-Questions/A-level/temperature.py
@@ -0,0 +1,8 @@
+'''
+Temperature of a city in Fahrenheit degrees is input through 
+the keyboard. Write a program to convert this temperature 
+into Centigrade degrees. 
+'''
+temp= eval(input("Enter the temperature of the City(in fahrenheit):"))
+cd= temp-32*5/9
+print("Temperature in Centigrade=",cd)
